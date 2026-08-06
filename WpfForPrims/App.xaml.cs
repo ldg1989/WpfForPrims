43c3df57bb5753dcdba6c405a4189341125f9be2
64148 <641489189@qq.com>
2026-08-06 22:46:05 +0800

    添加项目文件。

---
+++ WpfForPrims/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using Prism.DryIoc;
+using Prism.Ioc;
+using WpfForPrims.Views;
+
+namespace WpfForPrims
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        /// <summary>
+        /// 设置启动页
+        /// </summary>
+        /// <returns></returns>
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        /// <summary>
+        /// 注入服务
+        /// </summary>
+        /// <param name="containerRegistry"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // 名称要和 ViewModel 里 RequestNavigate 的字符串一致
+            containerRegistry.RegisterForNavigation<UCA>();
+            containerRegistry.RegisterForNavigation<UCB>();
+            containerRegistry.RegisterForNavigation<UCC>();
+        }
+    }
+}
---
+++ WpfForPrims/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace WpfForPrims.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public DelegateCommand<string> ShowContentCmm { get; set; }
+
+        private readonly IRegionManager _regionManager;
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            ShowContentCmm = new DelegateCommand<string>(ShowContentFunc);
+        }
+
+        private void ShowContentFunc(string viewName)
+        {
+            // 字符串 "UCA" / "UCB" / "UCC" 必须和 App.RegisterForNavigation 注册的类型一一对应
+            _regionManager.RequestNavigate("ContentRegion", viewName);
+        }
+    }
+}
