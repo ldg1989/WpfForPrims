7d40feafbaf4276d26c2f4581bf95660f98ec3dc
64148 <641489189@qq.com>
2026-08-06 22:49:30 +0800

    12

--- WpfForPrims/ViewModels/MainWindowViewModel.cs
+++ WpfForPrims/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,20 @@
-﻿using Prism.Commands;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
+using WpfForPrims.Views;
 
 namespace WpfForPrims.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
         public DelegateCommand<string> ShowContentCmm { get; set; }
+
 
         private readonly IRegionManager _regionManager;
 
@@ -16,10 +24,53 @@ namespace WpfForPrims.ViewModels
             ShowContentCmm = new DelegateCommand<string>(ShowContentFunc);
         }
 
+        /// <summary>
+        /// prism的实现 的实现
+        /// </summary>
+        /// <param name="viewName"></param>
         private void ShowContentFunc(string viewName)
         {
             // 字符串 "UCA" / "UCB" / "UCC" 必须和 App.RegisterForNavigation 注册的类型一一对应
             _regionManager.RequestNavigate("ContentRegion", viewName);
         }
+
+
+        #region 没使用 prism的实现
+        //private void ShowContentFunc(string viewName)
+        //{
+        //    if (viewName == "UCA")
+        //    {
+        //        ShowContent = new UCA();
+        //    }
+        //    if (viewName == "UCB")
+        //    {
+        //        ShowContent = new UCB();
+        //    }
+        //    if (viewName == "UCC")
+        //    {
+        //        ShowContent = new UCC();
+        //    }
+        //}
+
+
+        ///// <summary>
+        ///// 显示内容
+        ///// </summary>
+        //private UserControl _showContent;
+
+        //public UserControl ShowContent
+        //{
+        //    get { return _showContent; }
+        //    set
+        //    {
+        //        _showContent = value;
+        //        RaisePropertyChanged();
+        //    }
+        //}
+        #endregion
+
+
+
+
     }
 }
